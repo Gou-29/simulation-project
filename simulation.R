e7b18740a63c6c20c8bda8ea0290c7841aeca97d
yingj2622 <yj2622@cumc.columbia.edu>
2021-02-16 11:13:16 +0800

    simulation.r code

---
+++ simulation.R
@@ -0,0 +1,8 @@
+# Simulation 1.R 
+
+###### Using function in different R scripts ########
+
+source("Generator.R", encoding = 'utf-8')
+simple_fun("hello word")
+
+#####################################################
