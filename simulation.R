f6ae22c5ad9710b289e556aca9343bcb09413b31
yingj2622 <yj2622@cumc.columbia.edu>
2021-02-17 21:18:24 +0800

    delete the old version of data simulation

--- simulation.R
+++
@@ -1,8 +0,0 @@
-# Simulation 1.R 
-
-###### Using function in different R scripts ########
-
-source("generator.R", encoding = 'utf-8')
-simple_fun("hello word")
-
-#####################################################
